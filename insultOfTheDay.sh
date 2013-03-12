a793b484966b0f28aa2773099e75e919f8afdf92
Thomas Cousin <thomas.cousin@me.com>
2013-03-12 00:23:34 +0000

    Insult Of The Day Bug Fix

--- insultOfTheDay.sh
+++ insultOfTheDay.sh
@@ -2,4 +2,4 @@
 #Writen By Thomas Cousin
 #No responsabilities are taken for any damages or harm caused by this scrip, all risks are taken on by the user.
 
-echo &(curl --silent "https://api.twitter.com/1/statuses/user_timeline.rss?screen_name=randomfactaday" | grep "<title>" | sed -e 's/    <title>randomfactaday: //g' | sed -e 's/<\/title>//g' | sed -e 's/&quot;/"/g' | sed -e 's/    <title>Twitter \/ randomfactaday//g' | awk 'NR!=1') > /tmp/factOfTheDay.txt
+echo &(curl --silent "https://api.twitter.com/1/statuses/user_timeline.rss?screen_name=insultoftheday" | grep "<title>" | sed -e 's/    <title>insultoftheday: //g' | sed -e 's/<\/title>//g' | sed -e 's/&quot;/"/g' | sed -e 's/    <title>Twitter \/ randomfactaday//g' | awk 'NR!=1') > /tmp/insultOfTheDay.txt
