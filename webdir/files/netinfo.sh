38f7a8bdcde69a60dea65d51b75b45d895fdafd5
Max Maton <me@mmaton.com>
2013-03-13 16:49:23 +0000

    reuploading webdir

--- webdir/files/netinfo.sh
+++
@@ -1,5 +0,0 @@
-arp=$(arp-scan -l | awk 'NR > 2' | head -n -3)
-organise=$(echo "$arp" | sed 's/	/\n/g')
-numhosts=$(echo "$arp" | wc -l)
-echo "$numhosts" >> /var/www/default/files/hostc.txt
-echo "$organise" > /var/www/default/files/net.txt
