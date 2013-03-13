bbd4f03fbcea1f9c3ec855bd14b84bcba3d75c2e
Max Maton <me@mmaton.com>
2013-03-13 16:39:53 +0000

    modified:   netscan.sh

--- netscan.sh
+++ netscan.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 date=$(date +%d/%m/%H\ %k:%M:%S)
-arp-scan -l | awk 'NR > 2' | head -n -3 | awk '{print $1, $2}' | \
-while read ips mac; do
+arp=$(arp-scan -l)
+numhosts=$(echo "$arp" | awk 'NR > 2' | head -n -3 | wc -l)
+#########
 mysql --host=127.0.0.1 --user=status --password=status 734status << EOF
-insert into network (Date,IPAddress,MACAddress) values('$date','$ips','$mac');
+DELETE FROM current WHERE 1;
 EOF
-done 
+#########
+echo "$arp" | awk 'NR > 2' | head -n -3 | awk '{print $1, $2, $3}' | sed 's/[,]//g' | \
+#########
+while read IP mac des; do
+mysql --host=127.0.0.1 --user=status --password=status 734status << EOF
+insert into network (Date,IP,MACAddress,Description) values('$date', '$IP', '$mac', '$des') on duplicate key update MACAddress = '$mac';
+insert into current (Date,IP,MACAddress,Description) values('$date', '$IP', '$mac', '$des') on duplicate key update MACAddress = '$mac';
+EOF
+done
+#########
+echo "$numhosts" >> /var/www/default/files/hostc.txt
